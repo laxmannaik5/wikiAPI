4a8404d243bde08ad46c7a007141aaf05944e64b
laxmannaik5 <laxmannaik5055@gmail.com>
2021-09-03 03:11:44 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,160 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/wikiDB", { useNewUrlParser: true, useUnifiedTopology: true});
+
+const articleSchema = {
+  title: String,
+  content: String
+};
+
+const Article = mongoose.model("Article", articleSchema);
+
+
+/////// Request targetting all articles /////////
+
+app.route("/articles")
+
+.get(function(req, res){
+  Article.find(function(err, foundArticles){
+    if(!err){
+     res.send(foundArticles);
+   }else {
+     res.send(err);
+   }
+  });
+})
+
+.post(function(req, res){
+
+  const newArticle = new Article({
+    title: req.body.title,
+    content: req.body.content
+  });
+  newArticle.save(function(err){
+    if(!err){
+      res.send("Successfully added a new article");
+    }else {
+      res.send(err);
+    }
+  });
+})
+
+.delete(function(req, res){
+  Article.deleteMany(function(err){
+    if(!err){
+      res.send("Successfully deleted all articles.");
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+
+// app.get("/articles", function(req, res){
+//   Article.find(function(err, foundArticles){
+//     if(!err){
+//      res.send(foundArticles);
+//    }else {
+//      res.send(err);
+//    }
+//   });
+// });
+
+// app.post("/articles", function(req, res){
+//
+//   const newArticle = new Article({
+//     title: req.body.title,
+//     content: req.body.content
+//   });
+//   newArticle.save(function(err){
+//     if(!err){
+//       res.send("Successfully added a new article");
+//     }else {
+//       res.send(err);
+//     }
+//   });
+// });
+//
+// app.delete("/articles", function(req, res){
+//   Article.deleteMany(function(err){
+//     if(!err){
+//       res.send("Successfully deleted all articles.");
+//     } else {
+//       res.send(err);
+//     }
+//   });
+// });
+
+
+
+
+
+
+
+//// REQUEST TARGETTING SPECIFIC ARTICLES ////////
+
+app.route("/articles/:articleTitle")
+
+.get(function(req, res){
+  Article.findOne({title: req.params.articleTitle}, function(err, foundArticle){
+    if (foundArticle){
+      res.send(foundArticle);
+    }else{
+      res.send("No articles matching that title was found.");
+    }
+  });   //// to get Laxman Naik title we have to write Laxman%20Naik....i.e %20 represents space....
+})
+
+.put(function(req, res){
+  Article.update(
+    {title: req.params.articleTitle},
+    {title: req.body.title, content: req.body.content},
+    {overwrite: true},
+    function(err){
+      if (!err){
+        res.send("Successfully updated article.");
+      }
+    }
+  );
+})
+
+.patch(function(req, res){
+  Article.update(
+    {title: req.params.articleTitle},
+    {$set: req.body},
+    function(err){
+      if (!err){
+        res.send("Successfully updated article.")
+      }else {
+        res.send(err);
+      }
+    }
+  );
+})
+
+.delete(function(req, res){
+  Article.deleteOne({title: req.params.articleTitle}, function(err){
+    if (!err){
+      res.send("Successfully deleted the specific article.");
+    }else {
+      res.send(err);
+    }
+  });
+});
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
